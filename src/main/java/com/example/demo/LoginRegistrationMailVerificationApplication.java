30aca6596b37280d66e2e2be80043133a9ff5fd7
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2021-01-18 13:54:34 +0300

    DB setup and User entity

---
+++ src/main/java/com/example/demo/LoginRegistrationMailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationMailVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegistrationMailVerificationApplication.class, args);
+    }
+
+}
